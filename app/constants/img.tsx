97ef930719aec4c4a3dbaef05a615a7124de0019
chegecolins <161986914+chegecolins@users.noreply.github.com>
2024-03-19 08:07:32 +0000

    Update img.tsx

--- app/constants/img.tsx
+++ app/constants/img.tsx
@@ -2,5 +2,5 @@
 import { Id } from "@/convex/_generated/dataModel";
 
 export function getFileUrl(fileId: Id<"_storage">): string {
-  return `${process.env.NEXT_PUBLIC_CONVEX_URL}/api/storage/${fileId}`;
+  return `https://energized-dalmatian-74.convex.cloud/api/storage/${fileId}`;
 }
